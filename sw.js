f898c0d2f946d1ee81045f641c5f404cb1239131
amd5500gt <jasss.pb03@gmail.com>
2026-01-03 07:28:38 +0530

    update short ads

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10410186
+    "zoneId": 10410253
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
